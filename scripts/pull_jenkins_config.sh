c7077d296c882308d7d82c33285895b99f1bad90
Alexander Calhoun <acalhoun@redhat.com>
2018-05-22 11:20:32 -0400

    Updated screate to pull job folder and list all jobs being pulled.

--- scripts/pull_jenkins_config.sh
+++ scripts/pull_jenkins_config.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
+  echo "Pulling jobs:"
 for i in `ls /var/lib/jenkins/jobs/`; do 
 	echo $i 
-	mkdir /automated_ceph_test/jenkins_config/jobs/$i
-	cp /var/lib/jenkins/jobs/$i/config.xml /automated_ceph_test/jenkins_config/jobs/$i/
 done
+
+	cp  -r /var/lib/jenkins/jobs/ /automated_ceph_test/jenkins_config/jobs/
+
+
