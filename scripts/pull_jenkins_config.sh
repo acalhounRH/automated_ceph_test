6aaf2d7c7584bf8154f91bbbb46e2520d090ee72
Alexander Calhoun <acalhoun@redhat.com>
2018-05-23 15:11:45 -0400

    update to jobs files to account for dynamic script location

--- scripts/pull_jenkins_config.sh
+++ scripts/pull_jenkins_config.sh
@@ -2,9 +2,13 @@
 
   echo "Pulling jobs:"
 for i in `ls /var/lib/jenkins/jobs/`; do 
-	echo $i 
+	echo $i
+
+  mkdir -p /automated_ceph_test/jenkins_config/jobs/$i/
+  cp  -r /var/lib/jenkins/jobs/$i/config.xml /automated_ceph_test/jenkins_config/jobs/$i/
 done
 
-	cp  -r /var/lib/jenkins/jobs/ /automated_ceph_test/jenkins_config/jobs/
+  cp /var/lib/jenkins/config.xml /automated_ceph_test/jenkins_config/config.xml
+  sed -i '/<useSecurity>/,/<\/useSecurity>/s/true/false/' /automated_ceph_test/jenkins_config/config.xml
 
 
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo $HOME_DIR
