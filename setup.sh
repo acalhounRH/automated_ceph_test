972f9b293cc862154ca6c5ea316f9167ebd1b546
Alexander Calhoun <acalhoun@redhat.com>
2018-05-23 15:44:37 -0400

    update setup.sh script with suggested installation steps.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#get home directory 
 HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 echo $HOME_DIR
+
+#TODO for each job file update the script dir and change it to the user's install location
+
+#TODO install jenkins version 110 discovered that jobs are not backwards compatible 
+rpm -i https://pkg.jenkins.io/redhat/jenkins-2.110-1.1.noarch.rpm
+
+#TODO maybe install elasticsearch and grafana, alter ask user if they want to install ES and grafana
+
+#install pbench web server 
+
+
+
+
