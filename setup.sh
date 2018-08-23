0062a06df6f837c441dbd1fd9d95ee39cdbf5f43
acalhounRH <acalhoun@redhat.com>
2018-08-23 11:25:26 -0400

    changed pip install of urllib3

--- setup.sh
+++ setup.sh
@@ -49,7 +49,8 @@ fi
 
 sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/epel.repo
 yum install python-virtualenv -y
-yum install python-pip -y 
+yum install python-pip -y
+pip install pip -U
 pip install elasticsearch 
 pip install urllib3==1.23
 pip install statistics
