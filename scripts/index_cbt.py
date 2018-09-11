b526048828108aa23c58af062658dc2a4d077ab5
acalhounRH <acalhoun@redhat.com>
2018-09-11 15:04:00 -0400

    +

--- scripts/index_cbt.py
+++ scripts/index_cbt.py
@@ -36,8 +36,8 @@ def main():
     res_beg, res_end, res_suc, res_dup, res_fail, res_retry  = streaming_bulk(es, process_data_generator(test_id))
     
     #(beg, end, successes, duplicates, failures, retries_tracker['retries'])
-    
-    print time.strftime('%Y-%m-%dT%H:%M:%SGMT', gmtime(res_beg)), time.strftime('%Y-%m-%dT%H:%M:%SGMT', gmtime(res_end)), res_suc, res_dup, res_fail, res_retry
+    tdelta = datetime.strptime(gmtime(res_end), FMT) - datetime.strptime(gmtime(res_beg), FMT)
+    print tdelta, res_suc, res_dup, res_fail, res_retry
 
 
 #########################################################################
