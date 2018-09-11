e0dfc85cc4eaf26d96da27fdd7473eba83823b90
acalhounRH <acalhoun@redhat.com>
2018-09-11 15:09:59 -0400

    +

--- scripts/index_cbt.py
+++ scripts/index_cbt.py
@@ -37,7 +37,7 @@ def main():
     
     #(beg, end, successes, duplicates, failures, retries_tracker['retries'])
     FMT = '%H:%M:%S'
-    tdelta = gmtime(res_end) - gmtime(res_beg)
+    tdelta = datetime.strftime(gmtime(res_end), FMT) - datetime.strftime(gmtime(res_beg), FMT)
     print tdelta, res_suc, res_dup, res_fail, res_retry
 
 
