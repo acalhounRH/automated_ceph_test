8983bb1d635d5f61372ddb2de39a51d6a1d40a18
acalhounRH <acalhoun@redhat.com>
2018-09-12 10:38:51 -0400

    +

--- scripts/index_cbt.py
+++ scripts/index_cbt.py
@@ -214,8 +214,10 @@ def process_CBT_fiologs(tdir, cbt_config_obj, test_metadata):
             #fiologdoc = copy.deepcopy(headerdoc)
             metadata = test_metadata
             jsonfile = "%s/json_%s.%s" % (tdir, os.path.basename(file).split('_', 1)[0], os.path.basename(file).split('log.', 1)[1])
-            metadata['ceph_benchmark_test']['common']['hardware']['hostname'] = os.path.basename(file).split('log.', 1)[1]
-            metadata['ceph_benchmark_test']['appication_config']['ceph_config']['ceph_node-type'] = cbt_config_obj.get_host_type(metadata['host'])
+            hostname = os.path.basename(file).split('log.', 1)[1]
+            
+            metadata['ceph_benchmark_test']['common']['hardware']['hostname'] = hostname
+            metadata['ceph_benchmark_test']['appication_config']['ceph_config']['ceph_node-type'] = cbt_config_obj.get_host_type(hostname)
             
 
             fiolog_evaluator_generator = fiolog_evaluator(file, jsonfile, metadata)
