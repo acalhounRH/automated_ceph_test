6eedfd6d4ba2f58e93fae2a445f44c2c9116a96e
acalhounRH <acalhoun@redhat.com>
2018-09-12 11:00:57 -0400

    +

--- scripts/index_cbt.py
+++ scripts/index_cbt.py
@@ -191,8 +191,8 @@ def process_CBT_Pbench_data(tdir, cbt_config_obj, test_metadata):
                         metadata['ceph_benchmark_test']['common']['hardware']['hostname'] = hostname
                         metadata['ceph_benchmark_test']['common']['hardware']['ipaddress'] = socket.gethostbyname(hostname)
                         metadata['ceph_benchmark_test']['application_config']['ceph_config']['ceph_node_type'] = cbt_config_obj.get_host_type(hostname)
-                        metadata['ceph_benchmark_test']['test_info']['tool'] = pfname.split("/")[6]
-                        metadata['ceph_benchmark_test']['test_info']['file_name'] = pfname.split("/")[8]
+                        metadata['ceph_benchmark_test']['common']['test_info']['tool'] = pfname.split("/")[6]
+                        metadata['ceph_benchmark_test']['common']['test_info']['file_name'] = pfname.split("/")[8]
                     
                         pb_evaluator_generator = pbench_evaluator(pfname, metadata)
                         yield pb_evaluator_generator
