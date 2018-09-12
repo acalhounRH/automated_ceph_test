f1839caf2dc7504a616fff139f8ff37dc4cd1975
acalhounRH <acalhoun@redhat.com>
2018-09-12 14:09:52 -0400

    "

--- scripts/index_cbt.py
+++ scripts/index_cbt.py
@@ -191,7 +191,7 @@ def process_CBT_Pbench_data(tdir, cbt_config_obj, test_metadata):
                         metadata = {}
                         metadata = test_metadata
                         
-                        hostname = pfname.split("/")[5]
+                        hostname = pfname.split("/")[-1]
                         metadata['ceph_benchmark_test']['common']['hardware']['hostname'] = hostname
                         metadata['ceph_benchmark_test']['common']['hardware']['ipaddress'] = socket.gethostbyname(hostname)
                         metadata['ceph_benchmark_test']['application_config']['ceph_config']['ceph_node_type'] = cbt_config_obj.get_host_type(hostname)
