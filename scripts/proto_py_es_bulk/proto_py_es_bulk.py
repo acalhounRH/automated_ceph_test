0db351bf5c17566cd3c3775f360f83e372585450
acalhounRH <acalhoun@redhat.com>
2018-09-14 12:22:39 -0400

    .

--- scripts/proto_py_es_bulk/proto_py_es_bulk.py
+++ scripts/proto_py_es_bulk/proto_py_es_bulk.py
@@ -94,7 +94,7 @@ def streaming_bulk(es, actions):
            if status == 409:
                if retry_count == 0:
                    # Only count duplicates if the retry count is 0 ...
-                   logger.warning("Duplicate record detected.")
+                   logger.debug("Duplicate record detected.")
                    logger.debug(json.dumps(action, indent=1))
                    duplicates += 1
                else:
