eb94432a6a943a5ffe2e86adfa40721e9c966648
acalhounRH <acalhoun@redhat.com>
2018-09-13 23:32:33 -0400

    .

--- scripts/scribes/cbt_pbench_scribe.py
+++ scripts/scribes/cbt_pbench_scribe.py
@@ -9,6 +9,12 @@ class pbench_transcriber:
     def __init__(self, csv_file, metadata):
         self.csv_file = csv_file
         self.metadata = metadata
+        
+    def debug_progress_logger(text):
+        if logger.level = DEBUG:
+        logging.debug(text)
+        print '\x1b[80D' + '\x1b[K'+ text,
+        sys.stdout.flush()
 
     def emit_actions(self):
         importdoc = {}
@@ -16,11 +22,19 @@ class pbench_transcriber:
         importdoc["_type"] = "pbenchdata"
         importdoc["_op_type"] = "create"
         importdoc['_source'] = self.metadata
-    
+        
+        logger.debug("Indexing %s" % self.csv_file)
+        file_progress = 0
+        total_progress = 0 
         with open(self.csv_file) as csvfile:
             readCSV = csv.reader(csvfile, delimiter=',')
             first_row = True
             col_ary = []
+            
+            for row in readCSV:
+                total_progress += 1 
+            
+            
             for row in readCSV:
                 if first_row:
                     col_num = len(row)
@@ -67,4 +81,7 @@ class pbench_transcriber:
                         if a:
                                 importdoc["_source"]['ceph_benchmark_test']["test_data"] = tmp_doc
                                 importdoc["_id"] = hashlib.md5(json.dumps(importdoc)).hexdigest()
-                                yield a 
+                                yield a
+                    current_progress = ((file_progress - total_progress) / total_progress) * 100 
+                    debug_progress_logger("indexing... %s" % current_progress)
+                    file_progress += 1
