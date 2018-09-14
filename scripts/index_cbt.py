1958f8c42d392b027105938782efef7222fd4d79
acalhounRH <acalhoun@redhat.com>
2018-09-14 00:42:46 -0400

    .

--- scripts/index_cbt.py
+++ scripts/index_cbt.py
@@ -222,10 +222,12 @@ def process_CBT_Pbench_data(tdir, cbt_config_obj, test_metadata):
                         metadata = test_metadata
                         
                         hostname = host
+                        tool = pfname.split("/")[-2]
                         metadata['ceph_benchmark_test']['common']['hardware']['hostname'] = hostname
                         metadata['ceph_benchmark_test']['common']['hardware']['ipaddress'] = socket.gethostbyname(hostname)
                         metadata['ceph_benchmark_test']['application_config']['ceph_config']['ceph_node_type'] = cbt_config_obj.get_host_type(hostname)
-                        metadata['ceph_benchmark_test']['common']['test_info']['tool'] = pfname.split("/")[-2]
+                        metadata['ceph_benchmark_test']['common']['test_info']['tool'] = tool
+                        logger.debug(tool)
                         metadata['ceph_benchmark_test']['common']['test_info']['file_name'] = os.path.basename(pfname)
                     
                         pb_transcriber_generator = cbt_pbench_scribe.pbench_transcriber(pfname, metadata)
