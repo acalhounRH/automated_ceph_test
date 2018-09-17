14801b8842c7c15444a28f5d0505233b0c72eaf3
acalhounRH <acalhoun@redhat.com>
2018-09-17 13:15:27 -0400

    .

--- scripts/analyzers/cbt_rados_analyzer.py
+++ scripts/analyzers/cbt_rados_analyzer.py
@@ -2,7 +2,7 @@ import os, sys, json, time, types, csv, copy
 import logging, statistics, yaml 
 import datetime, socket
 from scribes import *
-import analyzers
+from analyzers.cbt_pbench_analyzer import cbt_pbench_analyzer 
 
 logger = logging.getLogger("index_cbt")
 
