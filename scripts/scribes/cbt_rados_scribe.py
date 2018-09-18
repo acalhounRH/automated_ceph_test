f1f0897394435d00b82a47fad92688ae0d722357
acalhounRH <acalhoun@redhat.com>
2018-09-18 11:00:35 -0400

    .

--- scripts/scribes/cbt_rados_scribe.py
+++ scripts/scribes/cbt_rados_scribe.py
@@ -104,7 +104,7 @@ class rados_json_transcriber():
         print json.dumps(data, indent=1)
         importdoc["_source"]['ceph_benchmark_test']['test_data']['rados'] = json.loads(data)
         #print json.dumps(importdoc, indent=1)
-        
+        importdoc["_id"] = hashlib.md5(json.dumps(importdoc)).hexdigest()
         return importdoc 
         
                                 
