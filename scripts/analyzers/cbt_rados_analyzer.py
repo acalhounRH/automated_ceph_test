8fd7ef8af1a4b63da1965f40c4db7b2f327a09c4
acalhounRH <acalhoun@redhat.com>
2018-09-17 23:07:54 -0400

    .

--- scripts/analyzers/cbt_rados_analyzer.py
+++ scripts/analyzers/cbt_rados_analyzer.py
@@ -60,6 +60,7 @@ def analyze_cbt_rados_files(tdir, cbt_config_obj, metadata):
                 json_file = "json_%s" % fname
                 #strip out hostname or ipaddress
                 file_name = os.path.basename(fname)
+                print file_name
                 rados_instance, hostname = file_name.split(".", 2)[2:]
                 
                 metadata['ceph_benchmark_test']['common']['hardware'] = hostname
