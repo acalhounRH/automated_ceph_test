7583be498cdee4542379e13cda49e70670717944
acalhounRH <acalhoun@redhat.com>
2018-09-18 12:37:23 -0400

    .

--- scripts/scribes/cbt_rados_scribe.py
+++ scripts/scribes/cbt_rados_scribe.py
@@ -62,13 +62,14 @@ class rados_transcriber():
                         line_count = 0
                     else:
                         line_count += 1 
+                        
                     if not time_set:
                         mdate, mtime = i.split()[:2]
                         time_mark = "%sT%s" % (mdate, mtime)
                         mtimestruct = datetime.datetime.strptime(time_mark, '%Y-%m-%dT%H:%M:%S.%f')
                         start_time = mtimestruct - timedelta(seconds=20)
                         # After finding out the time transcribe the json output data
-                        rados_json_transcriber_obj = rados_json_transcriber(self.json_log, start_time, self.metadata)
+                        rados_json_transcriber_obj = rados_json_transcriber(self.json_log, start_time, copy.deepcopy(self.metadata))
                         rjt_record = rados_json_transcriber_obj.emit_action()
                         yield rjt_record
                         
