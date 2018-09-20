69064880660562bf51ef0b2f0fd3b65c9946e403
acalhounRH <acalhoun@redhat.com>
2018-09-19 20:14:22 -0400

    .

--- scripts/run2runcomparison.py
+++ scripts/run2runcomparison.py
@@ -74,7 +74,7 @@ class test_holder():
         
     def reset_offset(self, initial_time, index):
         
-            print json.dumps(self.offset_map, indent=1)
+            print self.offset_map
             if index in self.offset_map:
                 self.offset = self.offset_map['index']
             else:
