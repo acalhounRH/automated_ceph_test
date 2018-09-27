83e1c76f25976dd209cc4e064a9b85d5f126799d
acalhounRH <acalhoun@redhat.com>
2018-09-27 14:24:51 -0400

    .

--- scripts/ceph_config_info.py
+++ scripts/ceph_config_info.py
@@ -26,7 +26,10 @@ def main():
     print json.dumps(ceph_status, indent=1)
     print json.dumps(ceph_df, indent=1)
     
-    print "Total size is %s" % ceph_df['stats']['total_bytes']
+    total_storage_size = ceph_df['stats']['total_bytes']
+    print "Total size is %s" % total_storage_size
+    
+    print "volume size should be %s " % calculate_vol_size(total_storage_size)
    # cmd = json.dumps({"prefix": "osd tree", "format": "json"})
    # _, output, _ = cluster.mon_command(cmd, b'', timeout=6)
    # logger.debug(json.dumps(json.loads(output), indent=4))
@@ -56,6 +59,21 @@ class ceph_client():
             logger.exception("Error issuing command")
             sys.exit(1)
     
+    def calculate_vol_size(self, total_storage, clients=3, numb_vol=8):
+        """
+            This method will calculate the vol_size form the total storage, 
+            number of clients, and number of volumes per client. 
+            
+            this is a helper function that ensures the ceph cluster is populated at least to 50% of capacity 
+        """
+        fifty_percent_of_total = total_storage * .5
+        vol_size_bytes = ( (fifty_percent_of_total / clients ) / numb_vol )
+        
+        vol_size_megabytes = vol_size_bytes * 0.00000095367432
+        
+        return vol_size_megabytes
+        
+        
         
         
         
