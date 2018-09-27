03d2cde1cc6e0f434a302a36ef77f23410966a58
acalhounRH <acalhoun@redhat.com>
2018-09-27 18:58:43 -0400

    ;

--- scripts/ceph_config_info.py
+++ scripts/ceph_config_info.py
@@ -47,7 +47,7 @@ def main():
     
     new_modifer.modify_job_file()
     
-    print json.dumps(raw_ose_tree, indent=1)
+    print json.dumps(raw_osd_tree, indent=1)
    
    
    # print json.dumps(ceph_status, indent=1)
