776fa7c7436d57257231efb10253c6e732535f4d
acalhounRH <acalhoun@redhat.com>
2018-09-27 14:04:50 -0400

    .

--- scripts/ceph_config_info.py
+++ scripts/ceph_config_info.py
@@ -16,9 +16,15 @@ def main():
 
     new_client = ceph_client()
     
-    logger.debug(new_client.issue_command("osd tree")) 
+    raw_osd_tree = new_client.issue_command("osd tree") 
         
-    logger.debug(new_client.issue_command("status"))
+    ceph_status = new_client.issue_command("status")
+    
+    ceph_df = new_client.issue_command("df")
+    
+    print json.dumps(ceph_status, indent=1)
+    print json.dumps(ceph_status, indent=1)
+    print json.dumps(ceph_df, indent=1)
    # cmd = json.dumps({"prefix": "osd tree", "format": "json"})
    # _, output, _ = cluster.mon_command(cmd, b'', timeout=6)
    # logger.debug(json.dumps(json.loads(output), indent=4))
@@ -35,16 +41,19 @@ class ceph_client():
         except Exception as e:
             logger.exception("Connection error: %s" % e.strerror )
             sys.exit(1)
+            
+        self.osd_host_list = []
+        self.osd_list = []
     
     def issue_command(self, command):
         cmd = json.dumps({"prefix": command, "format": "json"})
         try:
             _, output, _ = self.cluster.mon_command(cmd, b'', timeout=6)
-            return json.dumps(json.loads(output), indent=4)
+            return json.loads(output)
         except Exception as e:
             logger.exception("Error issuing command")
             sys.exit(1)
-        
+    
         
         
         
