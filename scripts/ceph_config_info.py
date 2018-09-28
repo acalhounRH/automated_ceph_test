ac0fc9b2719126b98a578ee8c1aef581489d56fb
acalhounRH <acalhoun@redhat.com>
2018-09-28 10:20:41 -0400

    /

--- scripts/ceph_config_info.py
+++ scripts/ceph_config_info.py
@@ -85,6 +85,7 @@ def main():
         for line in output:
             seperated_line = line.split(" ")
             print seperated_line
+            
             if seperated_line[0].strip(":").isdigit():
                 print "found interface: %s" % seperated_line[1]
                 
