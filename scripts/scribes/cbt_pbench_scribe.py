2f6f0a1d6419f8361d13c2e7e57fe691c4d7b4a1
acalhounRH <acalhoun@redhat.com>
2018-10-04 11:41:03 -0400

    x

--- scripts/scribes/cbt_pbench_scribe.py
+++ scripts/scribes/cbt_pbench_scribe.py
@@ -24,8 +24,11 @@ class pbench_transcriber:
 
     def get_service_id(self, service_pid):
         instance = -1
+        print service_pid
         for child in self.host_info['children']:
+            print child
             if service_pid in child:
+                print "###############GOT IT###############"
                 instance = child['service_id']
         
         return instance
