af4e847219078d80f9ccb967ad7e4f424cacb307
Alexander Calhoun <acalhoun@redhat.com>
2018-10-10 09:50:31 -0400

    updated jenkins config files

--- scripts/pull_jenkins_config.sh
+++ scripts/pull_jenkins_config.sh
@@ -4,7 +4,7 @@
 for i in `ls /var/lib/jenkins/jobs/`; do 
 	echo $i
 
-  mkdir -p /automated_ceph_test/jenkins_config/jobs/$i/
+  mkdir -p ~/automated_ceph_test/jenkins_config/jobs/$i/
   cp  -r /var/lib/jenkins/jobs/$i/config.xml ~/automated_ceph_test/jenkins_config/jobs/$i/
 done
 
