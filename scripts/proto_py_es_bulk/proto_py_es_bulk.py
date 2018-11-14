0b8b2a66a18f125bc97f3e3091590a426a587e8a
acalhounRH <acalhoun@redhat.com>
2018-11-13 20:54:32 -0500

    moved debuging statment to before appending to deque

--- scripts/proto_py_es_bulk/proto_py_es_bulk.py
+++ scripts/proto_py_es_bulk/proto_py_es_bulk.py
@@ -56,15 +56,7 @@ def streaming_bulk(es, actions):
             assert '_index' in cl_action
             assert '_type' in cl_action
             assert _op_type == cl_action['_op_type']
-            logger.debug("*************** %s *************** %s" % (actions_counter, len(actions_deque)))
-            actions_deque.append((0, cl_action))   # Append to the right side ...
-            ## number of actions que counter ++
-            actions_counter += 1
-            if actions_counter >= 100000:
-                logger.debug(actions_counter)
-
-            yield cl_action
-            
+                       
             if len(actions_deque) == 1:
                 stop_time = time.time()
                 processing_duration = start_time - stop_time
@@ -72,6 +64,15 @@ def streaming_bulk(es, actions):
                 logger.debug("%s objects, with a processing duration of %s." % (actions_counter, processing_duration))
                 start_time = time.time() 
                 actions_counter = 0  
+                
+            actions_deque.append((0, cl_action))   # Append to the right side ...
+            ## number of actions que counter ++
+            actions_counter += 1
+            if actions_counter >= 100000:
+                logger.debug(actions_counter)
+
+            yield cl_action
+ 
             
             # if after yielding an action some actions appear on the retry deque
             # start yielding those actions until we drain the retry queue.
