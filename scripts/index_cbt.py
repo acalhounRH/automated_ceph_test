531d83e9a86866fe7ff2efaaed7d713917221743
acalhounRH <acalhoun@redhat.com>
2018-11-17 12:01:35 -0500

    commenting out archive level analyzer

--- scripts/index_cbt.py
+++ scripts/index_cbt.py
@@ -95,9 +95,9 @@ def process_data(test_id):
             if 'cbt_config.yaml' in fname:
                 logger.info("Gathering cbt configuration settings...")
                 cbt_config_gen = cbt_config_scribe.cbt_config_transcriber(test_id, fname)
-                benchmark_name = cbt_config_gen.config['benchmarks']
-                args = (dirpath, cbt_config_gen, test_metadata, "archive")
-                analyzer_obj = factory.factory(benchmark_name, args)
+#                 benchmark_name = cbt_config_gen.config['benchmarks']
+#                 args = (dirpath, cbt_config_gen, test_metadata, "archive")
+#                 analyzer_obj = factory.factory(benchmark_name, args)
                 
                 return analyzer_obj             
 #                 yield cbt_config_gen
