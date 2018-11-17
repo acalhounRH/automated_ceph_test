fe098f1cac091e3b4b7b98c456cdae80d6d91739
acalhounRH <acalhoun@redhat.com>
2018-11-17 11:51:47 -0500

    corrected object init

--- scripts/analyzers/analyzer_factory.py
+++ scripts/analyzers/analyzer_factory.py
@@ -7,15 +7,17 @@ import logging
 logger = logging.getLogger("index_cbt")
 
 class analyzer_factory():
+    
+    def __init__(self):
         _analyzer_mapping = {"librbdfio" : cbt_fio_analyzer.analyze_cbt_fio_results,
                           "rados": cbt_rados_analyzer.analyze_cbt_rados_results}
-        @staticmethod
-        def factory(self, benchmark_name, **kwargs):
-            try:
-                return _analyzer_mapping[benchmark_name](**kwargs)
-            except KeyError:
-                raise FactoryError(benchmark_name, "Unkown benchmark")
-                
+    @staticmethod
+    def factory(self, benchmark_name, **kwargs):
+        try:
+            return _analyzer_mapping[benchmark_name](**kwargs)
+        except KeyError:
+            raise FactoryError(benchmark_name, "Unkown benchmark")
             
-        def register(self, benchmark_name, analyzer_object):
-            _analyzer_mapping[benchmark_name] = analyzer_object
+        
+    def register(self, benchmark_name, analyzer_object):
+        _analyzer_mapping[benchmark_name] = analyzer_object
