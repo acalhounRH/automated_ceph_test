7d244947eff5162ce65199a0aafaa28ef2ab551a
acalhounRH <acalhoun@redhat.com>
2018-11-17 21:00:29 -0500

    removed self from factory method call

--- scripts/analyzers/analyzer_factory.py
+++ scripts/analyzers/analyzer_factory.py
@@ -12,7 +12,7 @@ _analyzer_mapping = {"librbdfio" : cbt_fio_analyzer.analyze_cbt_fio_results,
 class analyzer_factory():
         
     @staticmethod
-    def factory(self, benchmark_name, dirpath, cbt_config_gen, test_metadata, type):
+    def factory(benchmark_name, dirpath, cbt_config_gen, test_metadata, type):
         try:
             logger.debug(benchmark_name)
             logger.debug("")
