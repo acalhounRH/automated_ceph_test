585470f9f5e38a27eb69b142d4d0fb27bd7940be
acalhounRH <acalhoun@redhat.com>
2018-11-17 21:04:05 -0500

    changed args to benchmark_name

--- scripts/analyzers/analyzer_factory.py
+++ scripts/analyzers/analyzer_factory.py
@@ -17,7 +17,7 @@ class analyzer_factory():
             logger.debug(benchmark_name)
             logger.debug("")
             logger.debug(dirpath, cbt_config_gen, test_metadata, type)
-            return _analyzer_mapping[args](dirpath, cbt_config_gen, test_metadata, type)
+            return _analyzer_mapping[benchmark_name](dirpath, cbt_config_gen, test_metadata, type)
         except KeyError:
             raise FactoryError(benchmark_name, "Unkown benchmark")
             
