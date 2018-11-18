b659d7327bd60a13026cb82827123ec525b147e6
acalhounRH <acalhoun@redhat.com>
2018-11-17 20:51:15 -0500

    change str to var

--- scripts/analyzers/analyzer_factory.py
+++ scripts/analyzers/analyzer_factory.py
@@ -12,18 +12,18 @@ _analyzer_mapping = {"librbdfio" : cbt_fio_analyzer.analyze_cbt_fio_results,
 class analyzer_factory():
         
     @staticmethod
-    def factory(self, benchmark_name, dirpath, cbt_config_gen, test_metadata, "benchmark"):
+    def factory(self, benchmark_name, dirpath, cbt_config_gen, test_metadata, type):
         try:
             logger.debug(benchmark_name)
             logger.debug("")
-            logger.debug(dirpath, cbt_config_gen, test_metadata, "benchmark")
+            logger.debug(dirpath, cbt_config_gen, test_metadata, type)
             
             for i in args:
                 print i
                 
             for i in kwargs:
                 print i
-            return _analyzer_mapping[args](dirpath, cbt_config_gen, test_metadata, "benchmark")
+            return _analyzer_mapping[args](dirpath, cbt_config_gen, test_metadata, type)
         except KeyError:
             raise FactoryError(benchmark_name, "Unkown benchmark")
             
