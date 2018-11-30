02dcc74be9cb466e111bba1e066864a9a6dee05b
Ben England <bengland@redhat.com>
2018-11-30 10:55:47 -0500

    Quick teardown (#16)
    * allow quick teardown without waiting teardown_delay min
    
    also tell user what to do and what to expect
    force cephfs unmounts if dangling mount left over
    
    * force unmount of any kernel Cephfs mountpoint
    
    do this prior to uninstalling Ceph RPMs
    save LINODE_GROUP file
    in case anything goes wrong with the linode removal
    
    * help the user

--- scripts/jobs/5-Teardown_Linode_Ceph_Cluster.sh
+++ scripts/jobs/5-Teardown_Linode_Ceph_Cluster.sh
@@ -10,16 +10,49 @@
 # Jenkins will stop the pipeline immediately,
 # this makes troubleshooting a pipeline much easier for the user
 
-(( teardown_sec = $teardown_delay_minutes * 60 ))
-echo "waiting $teardown_sec seconds before tear-down"
-sleep $teardown_sec
+echo "to suspend teardown indefinitely: touch /tmp/suspend-teardown"
+echo "to resume teardown:               rm /tmp/suspend-teardown"
+echo "to cause immediate teardown:      touch /tmp/teardown-now"
+echo "you must login to the remote agent from jenkins master to use this"
+echo "teardown script will check for these files every 60 seconds"
+echo ""
 
-while [ -f /tmp/suspend_teardown ] ; do
-	echo "waiting for /tmp/suspend_teardown file to be removed..."
-	sleep 60
+(( teardown_sec = $teardown_delay_minutes * 60 ))
+while [ $teardown_sec -gt 0 ] ; do
+    if [ -f /tmp/teardown-now ] ; then
+        echo "saw /tmp/teardown-now, starting tear-down now"
+        rm -fv /tmp/teardown-now
+        break
+    fi
+    sleep 60
+    (( teardown_sec = $teardown_sec - 60 ))
+    echo "waiting $teardown_sec seconds before tear-down"
 done
+
+# this feature enables script debugging for pipelines
+# you can't debug scripts once linodes are destroyed
+
+while [ -f /tmp/suspend-teardown ] ; do
+    echo "waiting for /tmp/suspend-teardown file to be removed..."
+    sleep 60
+done
+
+# first tear down Ceph on this agent 
+# so we don't have dangling mountpoints, etc
+
+umount -a -ff -t ceph
+yum remove -y ceph-common ceph-fuse librados2 ceph-ansible
+leftovers=`rpm -qa | awk '/ceph/||/librados/||/librbd/' | wc -l` 
+if [ $leftovers -gt 0 ] ; then
+    echo 'not all Ceph RPMS removed!'
+    rpm -qa | awk '/ceph/||/librados/||/librbd/'
+fi
+
+# now tear down the linode cluster
 
 cd $HOME/ceph-linode/
 virtualenv-2 linode-env && source linode-env/bin/activate && pip install linode-python
 export LINODE_API_KEY=$Linode_API_Key
-python ./linode-destroy.py
+python ./linode-destroy.py || exit 1
+echo "LINODE_GROUP = `cat ~/ceph-linode/LINODE_GROUP`"
+rm -fv ~/ceph-linode/LINODE_GROUP
