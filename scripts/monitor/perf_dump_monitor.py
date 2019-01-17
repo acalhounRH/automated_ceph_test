a91dc1769f7ca4000288da0f90469e595187faf6
acalhounRH <acalhoun@redhat.com>
2019-01-17 14:25:01 -0500

    fixed to get perf dump

--- scripts/monitor/perf_dump_monitor.py
+++ scripts/monitor/perf_dump_monitor.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import yaml, os, time, json, hashlib, paramiko
+import yaml, os, time, json, hashlib, paramiko, ast
 import socket, datetime, logging, rados, ipaddress
 from paramiko import SSHClient
 from datetime import date
@@ -28,7 +28,7 @@ def main():
         else:
             osd_host_dict[host["hostname"]].append(host["id"])
             
-    print json.dumps(osd_host_dict, indent=4)
+#    print json.dumps(osd_host_dict, indent=4)
     
     for host in osd_host_dict:
      collect_measurement(remoteclient, host, osd_host_dict[host], 1, 60, start_time)
@@ -44,37 +44,44 @@ def collect_measurement(remoteclient, host, osd_list, duration, time_interval, s
     
     elapsed_time = datetime.timedelta(seconds=0)
     print "working on host %s " % host
-    
-    while elapsed_time < datetime.timedelta(seconds=duration):
+    td_duration = datetime.timedelta(seconds=duration)
+    print td_duration.seconds
+    f = open("perf-dump.txt", 'w')
+    while elapsed_time.seconds < td_duration.seconds:
         collection_time = datetime.datetime.now() 
         elapsed_time = collection_time - start_time
+        print elapsed_time
         
         #collect the performance measurements 
         for osd in osd_list:
             print "osd %s" % osd
             perf_dump = remoteclient.issue_command(host, "ceph daemon osd.%s perf dump" % osd)   
+            #print perf_dump
+            f.write(json.dumps(perf_dump, indent=4))
             print json.dumps(perf_dump, indent=4)
         #sleep after you have collected perf dump
-        collection_delta_time = collection_time - datetime.datetime.now()
-        print collection_delta_time
-        print time_interval
-        print datetime.timedelta(seconds=time_interval)
-        print collection_delta_time.seconds
+        #collection_delta_time = collection_time - datetime.datetime.now()
+        collection_delta_time = datetime.datetime.now() - collection_time
+#        print collection_delta_time
+#        print time_interval
+#        print datetime.timedelta(seconds=time_interval)
+#        print collection_delta_time.seconds
         remainder = datetime.timedelta(seconds=time_interval) - collection_delta_time
         
-        print "going to sleep for %s" % remainder
+        print "going to sleep for %s" % remainder.seconds
         if remainder.seconds < 0:
             print "taking too ling"
             time.sleep(remainder.seconds)
         else: 
             time.sleep(remainder.seconds) #time_interval
+    f.close()
 
 class ssh_remote_command():
     def __init__(self):
           self.sshclient = SSHClient()
     
     def issue_command(self, host, command):
-        
+        combinded_ouput = []
         try:
             self.sshclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             key_path = os.path.expanduser("~/.ssh/authorized_keys")
@@ -82,8 +89,18 @@ class ssh_remote_command():
             stdin, stdout, stderr = self.sshclient.exec_command(command)
             
             output = stdout.readlines()
+
+            for line in output:
+                combinded_ouput.append(line.strip())
+            #newstuff = "[%s]" % ''.join(combinded_ouput)
+            newstuff = ''.join(combinded_ouput)
+#            print newstuff
             self.sshclient.close()
-            return output
+#            output = ''.join(output)
+#            output = output.strip()
+#            return output
+            brandnewstuff = ast.literal_eval(newstuff)
+            return brandnewstuff
         
         except Exception as e:
             self.sshclient.close()
@@ -122,8 +139,3 @@ class ceph_client():
             
 if __name__ == '__main__':
     main()
-
-
-
-
-
