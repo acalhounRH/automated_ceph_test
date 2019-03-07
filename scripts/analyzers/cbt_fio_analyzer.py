95a39e87ccaca634fa2feaad3e4baf9d683562c1
acalhounRH <acalhoun@redhat.com>
2019-03-07 11:44:33 -0500

    changed

--- scripts/analyzers/cbt_fio_analyzer.py
+++ scripts/analyzers/cbt_fio_analyzer.py
@@ -84,7 +84,8 @@ def analyze_cbt_fiologs(tdir, cbt_config_obj, test_metadata):
             metadata = test_metadata
             ### Changed pdsh to ansible and the format of the file names changed. 
             #jsonfile = "%s/json_%s.%s" % (tdir, os.path.basename(file).split('_', 1)[0], os.path.basename(file).split('log.', 1)[1])
-            jsonfile = "%s/json_%s" % os.path.abspath((tdir, os.path.basename(file).split('_', 1)[0]))
+            jsonfile = "%s/json_%s" % (tdir, os.path.basename(file).split('_', 1)[0])
+            jsonfile = os.path.abspath(jsonfile)
             hostname = os.path.basename(file).split('.')[2]
             
             metadata['ceph_benchmark_test']['common']['hardware']['hostname'] = hostname
