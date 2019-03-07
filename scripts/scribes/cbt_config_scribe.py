cdeb9b42d3eb75d9225861688d3d773b3cf917c8
acalhounRH <acalhoun@redhat.com>
2019-03-07 11:17:49 -0500

    reverted bencode change

--- scripts/scribes/cbt_config_scribe.py
+++ scripts/scribes/cbt_config_scribe.py
@@ -2,7 +2,6 @@
 import yaml, os, time, json, hashlib, paramiko
 import socket, datetime, logging, rados, ipaddress
 from paramiko import SSHClient
-import bencode
 from elasticsearch.client.remote import RemoteClient
 
 logger = logging.getLogger("index_cbt")
@@ -265,8 +264,7 @@ class cbt_config_transcriber:
         file_time = datetime.datetime.fromtimestamp(file_time)
         importdoc['_source']['date'] = file_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
         
-        #importdoc["_id"] = hashlib.md5(json.dumps(importdoc, sort_keys=True)).hexdigest()
-        importdoc["_id"] = hashlib.md5(bencode.bencode(importdoc)).hexdigest()
+        importdoc["_id"] = hashlib.md5(json.dumps(importdoc, sort_keys=True)).hexdigest()
         yield importdoc    
         
 
