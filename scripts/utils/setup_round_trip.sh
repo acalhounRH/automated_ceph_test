6512aee23d2e3edd716019360ad3802f0bf60378
acalhounRH <acalhoun@redhat.com>
2019-03-27 09:53:06 -0400

    created script to allow all host to ssh back to controller.

---
+++ scripts/utils/setup_round_trip.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+inventory_file=$1
+
+ARRAY=()
+for host in `ansible --list-hosts $i -i $inventory_file | grep -v hosts`; do
+	ping $j -c 1 > /dev/null 2>&1
+    exit_status=`echo $?`
+    if [ "$exit_status" -gt "0" ]; then
+    		host=`cat $inventory_file | grep $j | awk {'print $2'} | sed 's/.*=//'`		
+	echo $host
+    elif [ "$exit_status" -eq "0" ]; then
+    		host=$j
+	echo $host
+	fi
+	$ARRAY+=$host
+done
+
+host=`hostname`
+
+new_host="
+        Host $host
+        User root
+        UserKnownHostsFile /dev/null
+        StrictHostKeyChecking no
+        "
+
+for i in `cat tmp`; do
+        ssh $i "ssh-keygen -t rsa -q -N \"\" ; echo \"$new_host\" >> $HOME/.ssh/config " ;
+        pubkey=`ssh $i "cat ~/.ssh/id_rsa.pub"`
+        echo $pubkey>> ~/.ssh/authorized_keys
+done
+
+
+#for i in `cat tmp`; do
+#        ssh $i "echo \"$new_host\" >> $HOME/.ssh/config"
+#        pubkey=`ssh $i "cat ~/.ssh/id_rsa.pub"`
+#        echo $pubkey>> ~/.ssh/authorized_keys
+#done
