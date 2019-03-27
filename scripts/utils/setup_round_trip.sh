62a3e246b10eeac90a83b300672602cc7adb3a0f
acalhounRH <acalhoun@redhat.com>
2019-03-27 10:44:23 -0400

    corrected grep statement for host

--- scripts/utils/setup_round_trip.sh
+++ scripts/utils/setup_round_trip.sh
@@ -7,7 +7,7 @@ for host in `ansible --list-hosts $i -i $inventory_file | grep -v hosts`; do
 	ping $j -c 1 > /dev/null 2>&1
     exit_status=`echo $?`
     if [ "$exit_status" -gt "0" ]; then
-    		host=`cat $inventory_file | grep $j | awk {'print $2'} | sed 's/.*=//'`		
+    		host=`cat $inventory_file | grep $host | awk {'print $2'} | sed 's/.*=//'`		
 	echo $host
     elif [ "$exit_status" -eq "0" ]; then
     		host=$j
