d667178cf1f9bb048614183fa4624e12de3dbe3b
Alexander Calhoun <acalhoun@redhat.com>
2019-05-07 15:33:26 -0400

    disabled test mode

--- scripts/jobs/4-Perform_Analysis.sh
+++ scripts/jobs/4-Perform_Analysis.sh
@@ -16,7 +16,7 @@ pip install -I requests
 cd $archive_dir
 
 #if cbt
-	$script_dir/scripts/index_cbt.py -t $Test_ID -h $elasticsearch_host -p $elasticsearch_port -d -T -v
+	$script_dir/scripts/index_cbt.py -t $Test_ID -h $elasticsearch_host -p $elasticsearch_port -d
 #else if cosbench
 	#index_cosbench
 #else if 
