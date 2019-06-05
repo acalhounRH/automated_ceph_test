1ec7172ad0aa392e409a930eeb1f1b53ea0970ad
Alexander Calhoun <acalhoun@redhat.com>
2019-06-05 11:30:32 -0400

    added handle for K or B in bs value

--- scripts/scribes/cbt_fiojson_scribe.py
+++ scripts/scribes/cbt_fiojson_scribe.py
@@ -34,7 +34,9 @@ class fiojson_file_transcriber:
         block_size = tmp_doc['fio']['fio_json']['global_options']['bs']
         if 'k' in block_size:
              tmp_doc['fio']['fio_json']['global_options']['bs'] = (int(block_size.strip("k")) * 1000)
-        tmp_doc['fio']['fio_json']['global_options']['bs'] = ( int(tmp_doc['fio']['fio_json']['global_options']['bs'].strip('B')) / 1024)
+        elif 'B' in block_size:
+            tmp_doc['fio']['fio_json']['global_options']['bs'] = ( int(tmp_doc['fio']['fio_json']['global_options']['bs'].strip('B')) / 1024)
+        
         tmp_doc['fio']['fio_json']['timestamp_ms'] = json_doc['timestamp_ms']
         tmp_doc['fio']['fio_json']['timestamp'] = json_doc['timestamp']
         tmp_doc['fio']['fio_json']['fio_version'] = json_doc['fio version']
